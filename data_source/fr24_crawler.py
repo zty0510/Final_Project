15e8fab704a098965c9c88f5adcd56ce2aac6e00
zhangty3 <zhangty3@shanghaitech.edu.cn>
2020-12-15 20:56:02 +0800

    6

--- data_source/fr24_crawler.py
+++ data_source/fr24_crawler.py
@@ -11,9 +11,9 @@ import requests
 import csv
 import time
 import json
-# 引入 Beautiful Soup 模組
-from bs4 import BeautifulSoup
-import  re
+
+import re
+import os
 
 class Fr24Crawler:
 
@@ -23,71 +23,49 @@ class Fr24Crawler:
         self.loc=loc
         self.rng=rng
 
-
+        self.past_Airline_Quantity = 0 #过去飞机数量
+        self.now_Airline_Quantity = 0  # 现在飞机数量
         # raise NotImplementedError
 
-
     def get_square(self):
-        # jdc = abs(self.loc[1] - self.rng[1])  # 计算经度差
-        # if jdc > 180:  # 经度差调整
-        #     jdc = 360 - jdc
-        #
-        # maxlon = self.loc[1] + jdc
-        # minlon = self.loc[1] - jdc
-        # if minlon < -180:
-        #     minlon = minlon + 360
-        #
-        # ans = sorted([maxlon, minlon])
-        # minlon1 = ans[0]
-        # maxlon1 = ans[1]
-        #
-        # # 经度计算
-        #
-        # wdc = abs(self.rng[0] - self.loc[0])
-        # minlat = self.loc[0] - wdc
-        # maxlat = self.loc[0] + wdc
-        #
-        # if maxlat > 90 or minlat < -90:
-        #     print("illegal")
-        if not (-180 <= self.loc[1] <= 180 and -90 <= self.loc[0] <= 90):
-            raise ValueError("请输入正确的经纬度")
 
-        jdc = abs(self.loc[1] - self.rng[1])  # 计算经度差
+        if not (-180 <= self.loc[0] <= 180 and -90 <= self.loc[1] <= 90):
+            raise ValueError("请输入loc正确的经纬度")
+        if not (-180 <= self.rng[0] <= 180 and -90 <= self.rng[1] <= 90):
+            raise ValueError("请输入rng正确的经纬度")
+
+        if self.loc[1]>= self.rng[1]:
+            raise ValueError("rng应在loc的北侧")
+
+        jdc = abs(self.loc[0] - self.rng[0])  # 计算经度差
         if jdc < 180:
-            if self.loc[1] < 0:
-                if self.rng[1] > 0:  # 去除rng在loc的东侧
+            if self.loc[0] < 0:
+                if self.rng[0] > 0:  # 去除rng在loc的东侧
+                    raise ValueError('rng应该在loc的西侧')
+            if self.loc[0]>=0:
+                if self.rng[0]>=self.loc[0]:
                     raise ValueError('rng应该在loc的西侧')
         if jdc > 180:  # 经度差调整
-            if self.loc[1] > 0:
-                if self.rng[1] < 0:  # 去除rng在loc的东侧
+            if self.loc[0] > 0:
+                if self.rng[0] < 0:  # 去除rng在loc的东侧
                     raise ValueError('rng应该在loc的西侧')
             else:
                 jdc = 360 - jdc
-        if self.loc[1] < 0:
-            maxlon = self.loc[1] - jdc
-            minlon = self.loc[1] + jdc
-            if maxlon <= -180:  # 180度不分东西经
-                maxlon = maxlon + 360
-        if self.loc[1] > 0:
-            maxlon = self.loc[1] + jdc
-            minlon = self.loc[1] - jdc
-            if maxlon > 0:
-                maxlon = maxlon - 360
 
-        ans = sorted([maxlon, minlon])
-        minlon1 = ans[0]
-        maxlon1 = ans[1]
+        minlon = self.loc[0] - jdc
+        maxlon = self.loc[0] + jdc
+
 
         # 经度计算
 
-        wdc = abs(self.rng[0] - self.loc[0])
-        minlat = self.loc[0] - wdc
-        maxlat = self.loc[0] + wdc
+        wdc = abs(self.rng[1] - self.loc[1])
+        minlat = self.loc[1] - wdc
+        maxlat = self.loc[1] + wdc
 
-        if minlat<-90:           #若最小纬度越界，构不成方形
+        if minlat < -90:  # 若最小纬度越界，构不成方形
             raise ValueError("最小纬度越界")
 
-        return ([minlon1, maxlon1, minlat, maxlat])
+        return ([minlon, maxlon, minlat, maxlat])
 
 
     def get_data_once(self,updated_token):
@@ -98,7 +76,7 @@ class Fr24Crawler:
             'Referer': 'https: // zh.flightaware.com / live /',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh - CN, zh;q = 0.9, en - US;q = 0.8, en;q = 0.7'
-        }
+        } #请求头
         base_url = 'https://zh.flightaware.com/ajax/vicinity_aircraft.rvt?'
         token = updated_token  # 未来需要对token的更新进行处理
         param = '&minLon={minLon}&minLat={minLat}&maxLon={maxLon}&maxLat={maxLat}&token={token}'.format\
@@ -114,6 +92,8 @@ class Fr24Crawler:
     def spin(self, interval=1):
         while True:
             self.store_data()
+            print(self.get_Quantity_Binary_List())
+            print(self.get_change_bool())
             time.sleep(interval)
         raise NotImplementedError
 
@@ -126,15 +106,23 @@ class Fr24Crawler:
         return token[0]
 
     def store_data(self):
+
         data_source = self.get_data_once(updated_token=self.get_updated_token())#得到新的token并传入，爬取数据
+        self.update_Airline_Quantity(self.get_now_Airline_Quantity(),len(data_source['features'])) # 更新现在飞机数量
         #遍历 data，并做空值判断以及数据处理
+        path = os.path.abspath(".") + r"/csv_data.csv"  # 获取当前目录绝对路径
+        with open(path,"w+") as f:
+
+            f.write("latitude,longitude,heading,altitude,groundspeed,flight_number,departure_airport_IATA_CODE,arrival_airport_IATA_CODE")
+            f.write("\n")
+
 
         for i in range(1,len(data_source['features'])):
             if 'coordinates' not in data_source['features'][i]["geometry"]:
                 data_source['features'][i]["geometry"]['coordinates']=None
             else :
-                latitude = data_source['features'][i]["geometry"]['coordinates'][0]
-                longitude = data_source['features'][i]["geometry"]['coordinates'][1]
+                latitude = data_source['features'][i]["geometry"]['coordinates'][1]
+                longitude = data_source['features'][i]["geometry"]['coordinates'][0]
             if 'direction' not in data_source['features'][i]["properties"]:
                 heading = None
             else:
@@ -147,20 +135,17 @@ class Fr24Crawler:
                 groundspeed = None
             else:
                 groundspeed = data_source['features'][i]["properties"]['groundspeed']
-            if 'flight_number' not in data_source['features'][i]["properties"]:
+            if 'ident' not in data_source['features'][i]["properties"]:
                 flight_number = None
             else:
                 flight_number = data_source['features'][i]["properties"]['ident']
 
-            # heading = data_source['features'][i]["properties"]['direction']
-            #             # altitude = data_source['features'][i]["properties"]['altitude']
-            #             # groundspeed = data_source['features'][i]["properties"]['groundspeed']
-            #             # flight_number = data_source['features'][i]["properties"]['ident']
+
             departure_airport_IATA_CODE = data_source['features'][i]["properties"]['origin']['iata']
             arrival_airport_IATA_CODE = data_source['features'][i]["properties"]['destination']['iata']
             print(f'第{i}架飞机\n'
-                f'Latitude(经度):{latitude}\n'
-                  f'Longitude(纬度):{longitude}\n'
+                f'Latitude(纬度):{latitude}\n'
+                  f'Longitude(经度):{longitude}\n'
                   f'heading(航向）:{heading}\n'
                   f'altitude(海拔高度）:{altitude}\n'
                   f'groundspeed(地速):{groundspeed}\n'
@@ -168,11 +153,39 @@ class Fr24Crawler:
                   f'departure_airport_IATA_CODE(离开地机场代码):{departure_airport_IATA_CODE}\n'
                   f'arrival_airport_IATA_CODE(抵达机场代码):{arrival_airport_IATA_CODE}\n\n\n\n\n'
             )
-            # print(latitude,longitude,heading,altitude,groundspeed,flight_number,departure_airport_IATA_CODE,arrival_airport_IATA_CODE)
 
+            with open(path,"a+",newline="") as f:
+                writec = csv.writer(f)
+                row=[latitude,longitude,heading,altitude,groundspeed,flight_number,departure_airport_IATA_CODE,arrival_airport_IATA_CODE]
+                writec.writerow(row)
 
+    def set_now_Airline_Quantity(self,number):
+        self.now_Airline_Quantity = number
 
+    def get_now_Airline_Quantity(self):
+        return self.now_Airline_Quantity
 
+    def set_past_Airline_Quantity(self,number):
+        self.past_Airline_Quantity = number
 
-a = Fr24Crawler((-50,29),(-25,20))
-a.spin(interval=5)
+    def get_past_Airline_Quantity(self):
+        return self.past_Airline_Quantity
+
+    def update_Airline_Quantity(self,old,new):
+        self.set_past_Airline_Quantity(old)
+        self.set_now_Airline_Quantity(new)
+
+    def get_change_bool(self):##获取数量变换趋势
+        past_Quantity = self.get_past_Airline_Quantity()
+        now_Quantity = self.get_now_Airline_Quantity()
+        if now_Quantity > past_Quantity:
+            return 1                        #飞机数量增加，返回1
+        if now_Quantity == past_Quantity:
+            return 0                        #飞机数量不变，返回0
+        if now_Quantity < past_Quantity:
+            return -1                       #飞机数量减少，返回-1
+
+    def get_Quantity_Binary_List(self):##获取飞机数量的二进制list，list每一个值分别为 数字 的每一位
+        bin = '{:012b}'.format(self.get_now_Airline_Quantity())
+        bin_list = [int(x) for x in list(bin)]
+        return bin_list
