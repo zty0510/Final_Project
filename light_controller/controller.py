570431e71085fa2b62481c6c4611e61ba7ca0c53
zhangty3 <zhangty3@shanghaitech.edu.cn>
2020-12-15 22:09:20 +0800

    lalala

--- light_controller/controller.py
+++ light_controller/controller.py
@@ -1,22 +1,81 @@
-#=============================================================================#
+# =============================================================================#
 #                              Python Project                                 #
 #       SI 100B: Introduction to Information Science and Technology           #
 #                       Fall 2020, ShanghaiTech University                    #
 #                     Author: Diao Zihao <hi@ericdiao.com>                    #
 #                         Last motified: 07/07/2020                           #
-#=============================================================================#
+# =============================================================================#
+
+import gpiozero
+from time import sleep
 
 
 class BaseController:
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, red, yellow, green, blue):
+        self.red = gpiozero.LED(red)
+        self.yellow = gpiozero.LED(yellow)
+        self.green = gpiozero.LED(green)
+        self.blue = gpiozero.LED(blue)
 
-    def work_once(self):
-        raise NotImplementedError
+    def twinkling(self):
+        self.red.off()
+        freq = -1
+        while int(freq) <= 0:
+            freq = input("Please input frequency")
+        if int(freq) > 0:
+            self.red.blink(on_time=1 / (2 * int(freq)), off_time=1 / (2 * int(freq)))
 
-    def on(self):
-        raise NotImplementedError
+    def split1(self):
+        self.off()
+        self.red.on()
+        sleep(0.5)
+        self.red.off()
+        self.yellow.on()
+        sleep(0.5)
+        self.yellow.off()
+        self.green.on()
+        sleep(0.5)
+        self.green.off()
+        self.blue.on()
+        sleep(0.5)
+        self.blue.off()
+
+    def split2(self):
+        self.off()
+        self.blue.on()
+        sleep(0.25)
+        self.blue.off()
+        self.green.on()
+        sleep(0.25)
+        self.green.off()
+        self.yellow.on()
+        sleep(0.25)
+        self.yellow.off()
+        self.red.on()
+        sleep(0.25)
+        self.red.off()
+
+    def red_on(self):
+        self.red.on()
+
+
+    def blue_on(self):
+        self.blue.on()
+
+
+    def green_on(self):
+        self.green.on()
+
+
+    def yellow_on(self):
+        self.yellow.on()
+
+
 
     def off(self):
-        raise NotImplementedError
+        self.red.off()
+        self.blue.off()
+        self.green.off()
+        self.yellow.off()
+
--- state.py
+++ state.py
@@ -1,19 +1,91 @@
-#=============================================================================#
+# =============================================================================#
 #                              Python Project                                 #
 #       SI 100B: Introduction to Information Science and Technology           #
 #                       Fall 2020, ShanghaiTech University                    #
 #                     Author: Diao Zihao <hi@ericdiao.com>                    #
 #                         Last motified: 07/07/2020                           #
-#=============================================================================#
+# =============================================================================#
 from typing import Tuple
-from data_source.fr24_crawler import Fr24Crawler
-from light_controller.controller import *
+from fr24_crawler import Fr24Crawler
+import controller as cr
+import time
 
 
 class State:
+    def __init__(self, loc: Tuple[float, float], rng: float, red, yellow, green, blue):
+        self.data = Fr24Crawler(loc, rng)
+        self.num = self.data.get_Quantity_Binary_List()
+        self.change = self.data.get_change_bool()
+        self.control = cr.BaseController(red, yellow, green, blue)
 
-    def __init__(self):
+
+    def light(self):
+        self.control.split1()  # 间隔1 开始进行增减判断 同时标志后四位数据显示已经结束
+
+        if self.change > 0:
+            self.control.red_on()
+        elif self.change == 0:
+            self.control.yellow_on()
+        else:
+            self.control.green_on()
+
+        time.sleep(8)
+        self.control.split1()  # 间隔1 标志着增减判断结束
+        '''
+        前四位分割
+        '''
+
+        if self.num[0] == 1:
+            self.control.red_on()
+        if self.num[1] == 1:
+            self.control.yellow_on()
+        if self.num[2] == 1:
+            self.control.green_on()
+        if self.num[3] == 1:
+            self.control.blue_on()
+        time.sleep(8)
+        self.control.split2()  # 间隔2 标志着前四位数据显示已经完成
+        '''
+        middle 4 digits
+        '''
+        if self.num[4] == 1:
+            self.control.red_on()
+        if self.num[5] == 1:
+            self.control.yellow_on()
+        if self.num[6] == 1:
+            self.control.green_on()
+        if self.num[7] == 1:
+            self.control.blue_on()
+        time.sleep(8)
+        self.control.split2()  # 间隔2 标志着前四位数据显示已经完成
+
+        '''
+
+        后四位分割
+        '''
+        if self.num[8] == 1:
+            self.control.red_on()
+        if self.num[9] == 1:
+            self.control.yellow_on()
+        if self.num[10] == 1:
+            self.control.green_on()
+        if self.num[11] == 1:
+            self.control.blue_on()
+        time.sleep(8)
+        self.control.off()
+
+    def spin(self, interval=1):
+        while True:
+            self.data.store_data()
+            print(self.data.get_Quantity_Binary_List())
+            print(self.data.get_change_bool())
+            self.num = self.data.get_Quantity_Binary_List()
+            self.change = self.data.get_change_bool()
+            self.light()
+            time.sleep(interval)
+
         raise NotImplementedError
 
-    def spin(self):
-        raise NotImplementedError
+
+A = State((121, 31), (115, 45), 6, 13, 19, 26)
+A.spin(interval=5)
